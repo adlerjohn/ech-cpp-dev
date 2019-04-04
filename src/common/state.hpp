8b95762c33b1baf16141e6fb0e43028c8451ca2e
John Adler <john.adler@protonmail.ch>
2019-04-04 19:33:43 -0400

    Add skeleton files for state and transactions.

---
+++ src/common/state.hpp
@@ -0,0 +1,9 @@
+#ifndef ECH_CPP_STATE_HPP
+#define ECH_CPP_STATE_HPP
+
+class State {
+private:
+public:
+};
+
+#endif //ECH_CPP_STATE_HPP
---
+++ src/common/tx.hpp
@@ -0,0 +1,9 @@
+#ifndef ECH_CPP_TX_HPP
+#define ECH_CPP_TX_HPP
+
+class Tx {
+private:
+public:
+};
+
+#endif //ECH_CPP_TX_HPP
