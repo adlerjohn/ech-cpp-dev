47108c35a0368459de223c18004da4a540712d73
John Adler <john.adler@protonmail.ch>
2019-04-04 21:39:33 -0400

    Clean up doc comment.

--- src/common/color.hpp
+++ src/common/color.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 /**
- * Coins can be "colored" i.e., represent a token other than the base currency. Color is implemented as the address of
- * the token contract on the parent chain.
+ * Coins can be "colored" i.e., represent a token other than the base currency.
+ * Color is the address of the token contract on the parent chain.
  */
 class Color
 {
