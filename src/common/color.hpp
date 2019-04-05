3208f968de28de077791d0570fc9257fefe695bf
John Adler <john.adler@protonmail.ch>
2019-04-04 21:16:48 -0400

    Add doc for color class.

--- src/common/color.hpp
+++ src/common/color.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+/**
+ * Coins can be "colored" i.e., represent a token other than the base currency. Color is implemented as the address of
+ * the token contract on the parent chain.
+ */
 class Color
 {
 private:
