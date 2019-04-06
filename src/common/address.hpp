063d917a62c272e8b5108acd067edb8f1d2f6f00
John Adler <john.adler@protonmail.ch>
2019-04-06 11:10:37 -0400

    Add doc for address.

--- src/common/address.hpp
+++ src/common/address.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#define ADDRESS_BYTES 20
+
+/**
+ * Addresses are the last 20 bytes of the Keccak-256 of the hex
+ * representation of the public key.
+ */
 class Address
 {
 private:
