c8ec1d350ad5951c810b5ff08b8a46c3e084c199
John Adler <john.adler@protonmail.ch>
2019-04-11 12:30:00 -0400

    More cleanups.

--- src/crypto/keys.hpp
+++ src/crypto/keys.hpp
@@ -13,13 +13,13 @@ namespace ech::crypto
 class PublicKey : public byteset<64u>
 {
 public:
-	PublicKey(const std::string& str);
+	explicit PublicKey(const std::string& str);
 };
 
 class SecretKey : public byteset<32u>
 {
 public:
-	SecretKey(const std::string& str);
+	explicit SecretKey(const std::string& str);
 
 	auto sign(const Digest& msg) const;
 	auto sign(const std::string& str) const;
