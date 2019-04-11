ba29862379406c9acbbc55ee9e142a6a264f7c15
John Adler <john.adler@protonmail.ch>
2019-04-11 08:59:33 -0400

    Fix byteset test to not shouldfail.

--- test/crypto/byteset.cpp
+++ test/crypto/byteset.cpp
@@ -6,19 +6,19 @@
 using namespace ech::crypto;
 using Catch::Matchers::Equals;
 
-TEST_CASE("byteset length", "[byteset]")
+TEST_CASE("byteset length", "[crypto][byteset]")
 {
 	auto bs = byteset<1u>();
 	REQUIRE_THAT(std::string("00"), Equals(bs.toHex()));
 }
 
-TEST_CASE("byteset length, incorrect comparison", "[byteset][!shouldfail]")
+TEST_CASE("byteset length, incorrect comparison", "[crypto][byteset]")
 {
 	auto bs = byteset<1u>();
-	REQUIRE_THAT(std::string("0"), Equals(bs.toHex()));
+	REQUIRE_THAT(std::string("0"), !Equals(bs.toHex()));
 }
 
-TEST_CASE("byteset input", "[byteset]")
+TEST_CASE("byteset input", "[crypto][byteset]")
 {
 	auto str = std::string("01234567");
 	auto bs = byteset<4u>(str);
