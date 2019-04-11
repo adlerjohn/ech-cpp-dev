41f80f5f7f2e7daf761d5c5edb82c7f3dceac274
John Adler <john.adler@protonmail.ch>
2019-04-10 23:44:21 -0400

    Move byteset definition to header since it's a template class. Fix to hex method.

--- src/crypto/byteset.hpp
+++ src/crypto/byteset.hpp
@@ -16,11 +16,32 @@ class ByteSet
 private:
 	std::array<std::byte, N> _data;
 public:
-	ByteSet();
+	ByteSet()
+	{
+		this->_data.fill(std::byte(0));
+	}
 
-	explicit ByteSet(const std::string& hex);
+	explicit ByteSet(const std::string& hex)
+	{
+		// TODO dew it
+		// TODO need to assert that the size of hex matches with N
+		this->_data.fill(std::byte(0));
+	}
 
-	std::string toHex() const;
+	std::string toHex() const
+	{
+		static const auto hexes = std::string("0123456789abcdef");
+
+		auto offset = size_t(0);
+		auto str = std::string(N*2, '0');
+
+		for (auto& b : this->_data)	{
+			str.at(offset++) = hexes[(static_cast<uint8_t>(b) >> 4u) & 15u];
+			str.at(offset++) = hexes[static_cast<uint8_t>(b) & 15u];
+		}
+
+		return str;
+	}
 
 	friend std::ostream& operator<<(std::ostream& os, const ByteSet<N>& o)
 	{
