bc81fed7f522d32e6ef6dac0947d953a2120182d
John Adler <john.adler@protonmail.ch>
2019-04-11 01:31:23 -0400

    Fix creating byteset from hex string.

--- src/crypto/byteset.hpp
+++ src/crypto/byteset.hpp
@@ -15,9 +15,16 @@ template<uint64_t N>
 class ByteSet
 {
 private:
-
-
 	std::array<std::byte, N> _data;
+
+	auto hexCharToByte(const char c) const
+	{
+		static const std::string _hexes = std::string("0123456789abcdef");
+		if (std::find(_hexes.begin(), _hexes.end(), c) == _hexes.end())
+			throw std::invalid_argument("invalid hex char in position 1");
+		return (c >= 'A') ? (c - 'A') : (c - '0');
+	}
+
 public:
 	ByteSet()
 	{
@@ -26,8 +33,6 @@ public:
 
 	explicit ByteSet(const std::string& str)
 	{
-		static const std::string _hexes = std::string("0123456789abcdef");
-
 		auto raw_size = str.length();
 		if (raw_size % 2u != 0u)
 			throw std::invalid_argument("hex string must have an even number of hexes");
@@ -35,19 +40,12 @@ public:
 		if (size != N)
 			throw std::invalid_argument("attempting to convert from hex string with invalid size");
 
-		for (auto i = size_t(0); i < raw_size; i+=2) {
+		for (auto i = size_t(0); i < raw_size; i += 2u) {
 			auto pos1 = str.at(i);
-			auto pos2 = str.at(i+1);
+			auto pos2 = str.at(i + 1);
 
-			if(std::find(_hexes.begin(), _hexes.end(), pos1) == _hexes.end())
-				throw std::invalid_argument("invalid hex char in position 1");
-			if(std::find(_hexes.begin(), _hexes.end(), pos2) == _hexes.end())
-				throw std::invalid_argument("invalid hex char in position 2");
-
-			this->_data.at(i/2u, static_cast<std::byte>(pos1 * 16 + pos2));
+			this->_data.at(i / 2u) = static_cast<std::byte>(hexCharToByte(pos1) * 16u + hexCharToByte(pos2));
 		}
-
-		this->_data.fill(std::byte(0));
 	}
 
 	std::string toHex() const
@@ -64,6 +62,8 @@ public:
 
 		return str;
 	}
+
+	auto getData() const { return this->_data; }
 
 	friend std::ostream& operator<<(std::ostream& os, const ByteSet<N>& o)
 	{
--- test/crypto/byteset.cpp
+++ test/crypto/byteset.cpp
@@ -4,15 +4,23 @@
 #include "crypto/byteset.hpp"
 
 using namespace ech::crypto;
+using Catch::Matchers::Equals;
 
 TEST_CASE("byteset length", "[byteset]")
 {
 	auto bs = byteset<1u>();
-	REQUIRE(std::string("00").compare(bs.toHex()) == 0);
+	REQUIRE_THAT(std::string("00"), Equals(bs.toHex()));
 }
 
 TEST_CASE("byteset length, incorrect comparison", "[byteset][!shouldfail]")
 {
 	auto bs = byteset<1u>();
-	REQUIRE(std::string("0").compare(bs.toHex()) == 0);
+	REQUIRE_THAT(std::string("0"), Equals(bs.toHex()));
+}
+
+TEST_CASE("byteset input", "[byteset]")
+{
+	auto str = std::string("01234567");
+	auto bs = byteset<4u>(str);
+	REQUIRE_THAT(str, Equals(bs.toHex()));
 }
