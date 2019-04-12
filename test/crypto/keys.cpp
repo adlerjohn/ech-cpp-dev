b855bac2c058954b7ea1466b83970efccd878f93
John Adler <john.adler@protonmail.ch>
2019-04-11 20:00:20 -0400

    Enable test for key signing.

--- test/crypto/keys.cpp
+++ test/crypto/keys.cpp
@@ -14,7 +14,7 @@ TEST_CASE("key derivation (aleth example)", "[crypto][keys]")
 	REQUIRE_THAT("97466f2b32bc3bb76d4741ae51cd1d8578b48d3f1e68da206d47321aec267ce78549b514e4453d74ef11b0cd5e4e4c364effddac8b51bcfc8de80682f952896f", Equals(pk.toHex()));
 }
 
-/*
+
 TEST_CASE("key sign", "[crypto][keys]")
 {
 	auto sk = SecretKey("3ecb44df2159c26e0f995712d4f39b6f6e499b40749b1cf1246c37f9516cb6a4");
@@ -35,4 +35,3 @@ TEST_CASE("key sign", "[crypto][keys]")
 		REQUIRE_THAT(signedMessage, Equals(sig));
 	}
 }
-*/
