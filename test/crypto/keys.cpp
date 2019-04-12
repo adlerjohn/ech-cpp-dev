ae237b59e9d5c603d267a40be90f5972db4526d9
John Adler <john.adler@protonmail.ch>
2019-04-11 21:16:44 -0400

    Change to failing test for keys.

--- test/crypto/keys.cpp
+++ test/crypto/keys.cpp
@@ -2,6 +2,7 @@
 
 // Project includes
 #include "crypto/keys.hpp"
+#include "crypto/signature.hpp"
 
 using namespace ech::crypto;
 using Catch::Matchers::Equals;
@@ -18,21 +19,17 @@ TEST_CASE("key derivation (aleth example)", "[crypto][keys]")
 TEST_CASE("key sign", "[crypto][keys]")
 {
 	auto sk = SecretKey("3ecb44df2159c26e0f995712d4f39b6f6e499b40749b1cf1246c37f9516cb6a4");
-	auto pk = PublicKey(sk);
-
 	auto msg = std::string("The quick brown fox jumps over the lazy dog.");
-	auto signedMessage = std::string("fd9c0f5b84063479f7b6ff1ccf48dd1ba15c79e10224a5c1a47a3e46025c7e4a188a2f6ba4e109d562e536bd6b03caa83dfc4eba5aa03146f1f2f04404f952e31c");
 
-	// Signing is not deterministic so we can't test for validity, need to test in Signature
 	SECTION("sign message")
 	{
 		auto sig = sk.sign(msg);
-		//REQUIRE_THAT(signedMessage, Equals(sig));
+		REQUIRE(Signature::size() * 2 == sig.length());
 	}
 	SECTION("sign digest")
 	{
 		auto digest = Digest(msg);
 		auto sig = sk.sign(digest);
-		//REQUIRE_THAT(signedMessage, Equals(sig));
+		REQUIRE(Signature::size() * 2 == sig.length());
 	}
 }
