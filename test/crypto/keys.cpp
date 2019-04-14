e4fd2abc7726029ac929a808bb726fb8ae5220f0
John Adler <john.adler@protonmail.ch>
2019-04-14 13:53:50 -0400

    Cleanup.

--- test/crypto/keys.cpp
+++ test/crypto/keys.cpp
@@ -15,7 +15,6 @@ TEST_CASE("key derivation (aleth example)", "[crypto][keys]")
 	REQUIRE_THAT("97466f2b32bc3bb76d4741ae51cd1d8578b48d3f1e68da206d47321aec267ce78549b514e4453d74ef11b0cd5e4e4c364effddac8b51bcfc8de80682f952896f", Equals(pk.toHex()));
 }
 
-
 TEST_CASE("key sign", "[crypto][keys]")
 {
 	auto sk = SecretKey("3ecb44df2159c26e0f995712d4f39b6f6e499b40749b1cf1246c37f9516cb6a4");
