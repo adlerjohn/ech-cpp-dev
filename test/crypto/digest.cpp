bf364d10dbeadde095f99f33f26372bf206e8f2c
John Adler <john.adler@protonmail.ch>
2019-04-16 12:05:07 -0400

    Add test for digest with 'hello' string.

--- test/crypto/digest.cpp
+++ test/crypto/digest.cpp
@@ -23,3 +23,9 @@ TEST_CASE("digest testing", "[crypto][digest]")
 	auto dig = Digest(std::string("testing"));
 	REQUIRE_THAT("5f16f4c7f149ac4f9510d9cf8cf384038ad348b3bcdc01915f95de12df9d1b02", Equals(dig.toHex()));
 }
+
+TEST_CASE("digest hello", "[crypto][digest]")
+{
+	auto dig = Digest(std::string("hello"));
+	REQUIRE_THAT("1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8", Equals(dig.toHex()));
+}
