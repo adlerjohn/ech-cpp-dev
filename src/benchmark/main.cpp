f3377e3418dadcfb2a59c014b2d852c884ca410e
John Adler <john.adler@protonmail.ch>
2019-04-17 23:11:08 -0400

    Add benchmark bin.

---
+++ src/benchmark/main.cpp
@@ -0,0 +1,16 @@
+#include "main.hpp"
+
+// System includes
+#include <iostream>
+
+// Project includes
+#include "common/utxo.hpp"
+
+using namespace ech;
+
+int main()
+{
+	std::cout << "Hello, World!" << std::endl;
+
+	return 0;
+}
