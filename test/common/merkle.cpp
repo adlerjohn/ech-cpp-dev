c5a1b7a971ddd4a841b67eb0aa9b141a76e95ebd
John Adler <john.adler@protonmail.ch>
2019-04-20 21:29:23 -0400

    Fix unit tests for merkle.

--- test/common/merkle.cpp
+++ test/common/merkle.cpp
@@ -9,21 +9,19 @@ using Catch::Matchers::Equals;
 TEST_CASE("Merkle tree empty", "[common][merkle]")
 {
 	const auto zero = std::vector<std::byte>(crypto::Digest::size(), std::byte(0x00));
-	const auto digest = crypto::Digest(zero);
 
 	const auto root = MerkleTree(std::vector<crypto::Digest>());
 
-	REQUIRE_THAT(digest.toHex(), Equals(root.getRoot().toHex()));
+	REQUIRE_THAT("290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563", Equals(root.getRoot().toHex()));
 }
 
 TEST_CASE("Merkle tree two empty elements", "[common][merkle]")
 {
 	const auto zero = std::vector<std::byte>(crypto::Digest::size(), std::byte(0x00));
-	const auto digest = crypto::Digest(zero);
 	const auto e1 = crypto::Digest(zero);
 	const auto e2 = crypto::Digest(zero);
 
 	const auto root = MerkleTree(std::vector<crypto::Digest>{e1, e2});
 
-	REQUIRE_THAT(digest.toHex(), Equals(root.getRoot().toHex()));
+	REQUIRE_THAT("ad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5", Equals(root.getRoot().toHex()));
 }
