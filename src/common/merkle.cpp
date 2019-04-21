08b2a116c7f16823e50c263abbb196d2de6a5fa5
John Adler <john.adler@protonmail.ch>
2019-04-20 21:03:56 -0400

    Implement Merkle root calculation.

--- src/common/merkle.cpp
+++ src/common/merkle.cpp
@@ -5,10 +5,26 @@ using namespace ech;
 const crypto::Digest MerkleTree::calculateRoot(const std::vector<crypto::Digest>& leaves) const
 {
 	// Special case: if the tree is empty, return the hash of 0
-	if (leaves.empty())
-		return crypto::Digest("0000000000000000000000000000000000000000000000000000000000000000");
+	if (leaves.empty()) {
+		const auto zeroes = std::vector<std::byte>(32, std::byte(0x00));
+		return crypto::Digest(zeroes);
+	}
 
-	return crypto::Digest("");
+	auto hashesPrev = leaves;
+
+	while (hashesPrev.size() > 1) {
+		if (hashesPrev.size() % 2 == 1)
+			hashesPrev.push_back(hashesPrev.back());
+
+		std::vector<crypto::Digest> hashesNext;
+		hashesNext.reserve(hashesPrev.size() / 2);
+		for (size_t i = 0; i < hashesPrev.size() / 2; i++)
+			hashesNext.push_back(hashesPrev.at(i*2) + hashesPrev.at(i*2+1));
+
+		hashesPrev.swap(hashesNext);
+	}
+
+	return hashesPrev.front();
 }
 
 MerkleTree::MerkleTree(const std::vector<crypto::Digest>& leaves)
--- test/common/merkle.cpp
+++ test/common/merkle.cpp
@@ -8,8 +8,22 @@ using Catch::Matchers::Equals;
 
 TEST_CASE("Merkle tree empty", "[common][merkle]")
 {
-	const auto digest = crypto::Digest("0000000000000000000000000000000000000000000000000000000000000000");
+	const auto zeroes = std::vector<std::byte>(32, std::byte(0x00));
+	const auto digest = crypto::Digest(zeroes);
+
 	const auto root = MerkleTree(std::vector<crypto::Digest>());
+
+	REQUIRE_THAT(digest.toHex(), Equals(root.getRoot().toHex()));
+}
+
+TEST_CASE("Merkle tree two empty elements", "[common][merkle]")
+{
+	const auto zeroes = std::vector<std::byte>(32, std::byte(0x00));
+	const auto digest = crypto::Digest(zeroes);
+	const auto e1 = crypto::Digest(zeroes);
+	const auto e2 = crypto::Digest(zeroes);
+
+	const auto root = MerkleTree(std::vector<crypto::Digest>{e1, e2});
 
 	REQUIRE_THAT(digest.toHex(), Equals(root.getRoot().toHex()));
 }
