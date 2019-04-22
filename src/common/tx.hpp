f86b6f4d9a2af3b2f210eb892fada4749b22560a
John Adler <john.adler@protonmail.ch>
2019-04-21 23:13:49 -0400

    Add some transaction fields.

--- src/common/tx.hpp
+++ src/common/tx.hpp
@@ -19,22 +19,27 @@ class Tx : public Serializable
 private:
 	// Transaction ID
 	const TXID _id;
-
 	// Inputs to the transaction
 	const std::vector<UTXOID> _inputs;
-
 	// Outputs of the transaction
 	const std::vector<TXO> _outputs;
-
 	// Digital signatures, one for each input
 	const std::vector<crypto::Signature> _witnesses;
+	// Minimum height this transaction can be included in
+	// If 0, then any height is valid
+	const uint64_t _heightMin;
+	// Maximum height this transaction can be included in
+	// If 0, then any height is valid
+	const uint64_t _heightMax;
+	// If non-0, the height of a recent block that must exist
+	const uint64_t _recentBlockHeight;
+	// If recentBlockHeight is non-0, the hash of the recent block
+	const uint64_t _recentBlockHash;
+	// Replay attack protection
+	const uint8_t _chainID;
 
 	// TODO flags?
 	// TODO fee/max fee
-	// TODO chain ID
-	// TODO block height
-	// TODO block hash
-	// TODO extra data
 public:
 	Tx(const std::vector<UTXOID>& inputs, const std::vector<TXO>& outputs, const std::vector<crypto::Signature>& witnesses);
 
