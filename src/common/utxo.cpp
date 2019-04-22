226ebb0d303a908b856d827e93df48d38c529025
John Adler <john.adler@protonmail.ch>
2019-04-21 21:37:16 -0400

    Implement UTXO ID calculation.

--- src/common/utxo.cpp
+++ src/common/utxo.cpp
@@ -2,8 +2,27 @@
 
 using namespace ech;
 
+const std::vector<std::byte> UTXO::serializeData(const crypto::Address& owner, const CoinAmount& amount, const Color& color) const
+{
+	std::vector<std::byte> serial;
+
+	serial.insert(serial.end(), owner.begin(), owner.end());
+
+	static const size_t amountSize = 32u;
+	std::stringstream buf;
+	buf << std::hex << std::setfill('0') << std::setw(amountSize * 2) << _amount;
+	auto str = buf.str();
+	std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+	const auto amountBytes = crypto::byteset<amountSize>(str);
+	serial.insert(serial.end(), amountBytes.begin(), amountBytes.end());
+
+	serial.insert(serial.end(), color.serialize().begin(), color.serialize().end());
+
+	return serial;
+}
+
 UTXO::UTXO(const crypto::Address& owner, const CoinAmount& amount, const Color& color)
-	: _id(UTXOID(std::string(64u, '0')))
+	: _id(UTXOID(serializeData(owner, amount, color)))
 	, _owner(owner)
 	, _amount(amount)
 	, _color(color)
--- src/common/utxo.hpp
+++ src/common/utxo.hpp
@@ -17,26 +17,25 @@ class UTXO
 private:
 	// ID of the UTXO, determined by its hash
 	const UTXOID _id;
-
 	// Owner of the UTXO
 	const crypto::Address _owner;
-
 	// Amount of coins for this UTXO
 	const CoinAmount _amount;
-
 	// Color (if the UTXO represents a colored coin)
 	const Color _color;
+
+	[[nodiscard]] const std::vector<std::byte> serializeData(const crypto::Address& owner, const CoinAmount& amount, const Color& color) const;
 
 public:
 	UTXO(const crypto::Address& owner, const CoinAmount& amount, const Color& color);
 
-	auto getId() const { return this->_id; }
+	const auto& getId() const { return this->_id; }
 
-	auto getOwner() const { return this->_owner; }
+	const auto& getOwner() const { return this->_owner; }
 
-	auto getAmount() const { return this->_amount; }
+	const auto getAmount() const { return this->_amount; }
 
-	auto isColored() const { return this->_color.isColored(); }
+	const auto isColored() const { return this->_color.isColored(); }
 };
 
 } // namespace ech
