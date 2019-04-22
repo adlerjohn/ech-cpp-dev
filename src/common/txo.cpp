65b10c6bf6f3242801b3fbd401b1c2def27861b1
John Adler <john.adler@protonmail.ch>
2019-04-21 20:40:25 -0400

    Implement serialization of txo class.

--- src/common/txo.cpp
+++ src/common/txo.cpp
@@ -12,6 +12,22 @@ TXO::TXO(const uint32_t index, const crypto::Address recipient, const CoinAmount
 
 const std::vector<std::byte> TXO::serialize() const
 {
-	// TODO implement
-	return std::vector<std::byte>();
+	std::vector<std::byte> serial;
+
+	serial.push_back(static_cast<std::byte>(_index));
+
+	serial.insert(serial.end(), _recipient.begin(), _recipient.end());
+
+	static const size_t amountSize = 32u;
+	std::stringstream buf;
+	buf << std::hex << std::setfill('0') << std::setw(amountSize * 2) << _amount;
+	auto str = buf.str();
+	std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+	const auto amount = crypto::byteset<amountSize>(str);
+	serial.insert(serial.end(), amount.begin(), amount.end());
+
+	const auto color = _color.serialize();
+	serial.insert(serial.end(), color.begin(), color.end());
+
+	return serial;
 }
---
+++ test/common/txo.cpp
@@ -0,0 +1,42 @@
+#include "catch2/catch.hpp"
+
+// Project includes
+#include "common/txo.hpp"
+
+using namespace ech;
+using Catch::Matchers::Equals;
+
+TEST_CASE("txo zero", "[common][txo]")
+{
+	const auto index = 0;
+	const auto recipient = crypto::Address("0000000000000000000000000000000000000000");
+	const auto amount = CoinAmount(0);
+	const auto color = Color();
+
+	const auto txo = TXO(index, recipient, amount, color);
+
+	const auto golden = std::string(
+		"00"
+		"0000000000000000000000000000000000000000"
+		"0000000000000000000000000000000000000000000000000000000000000000"
+		"00");
+	REQUIRE_THAT(golden, Equals(txo.toHex()));
+}
+
+TEST_CASE("txo random", "[common][txo]")
+{
+	const auto index = 1;
+	const auto recipient = crypto::Address("deadbeefdeadbeefdeadbeefdeadbeefdeadbeef");
+	const auto amount = CoinAmount(42);
+	const auto address = crypto::Address("8a40bfaa73256b60764c1bf40675a99083efb075");
+	const auto color = Color(address);
+
+	const auto txo = TXO(index, recipient, amount, color);
+
+	const auto golden = std::string(
+		"01"
+		"deadbeefdeadbeefdeadbeefdeadbeefdeadbeef"
+		"000000000000000000000000000000000000000000000000000000000000002a"
+		"018a40bfaa73256b60764c1bf40675a99083efb075");
+	REQUIRE_THAT(golden, Equals(txo.toHex()));
+}
