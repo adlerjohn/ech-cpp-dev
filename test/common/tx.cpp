658a48fe48dc5f9dddd9e7c890ddcbee16b39929
John Adler <john.adler@protonmail.ch>
2019-04-22 13:59:37 -0400

    Add signature for unit test for tx. (This isn't a valid sig, just to check serialization.)

--- test/common/tx.cpp
+++ test/common/tx.cpp
@@ -53,8 +53,8 @@ TEST_CASE("tx random", "[common][tx]")
 	const auto txo = TXO(index, recipient, amount, color);
 	const auto outputs = std::vector<TXO>{txo};
 
-	// TODO add signature
-	const auto witnesses = std::vector<crypto::Signature>{};
+	const auto signature = crypto::Signature("5385685eef5fe0f2f93fa022002bb00f8b2478b9567618e47463cda72a78dcdb56a7a6bcd0bd79f709f15e0d3d53ba9909d4a3d100e25065bc0b7bd2537f39191c");
+	const auto witnesses = std::vector<crypto::Signature>{signature};
 
 	const uint64_t heightMin = 42u;
 	const uint64_t heightMax = 43u;
@@ -63,26 +63,31 @@ TEST_CASE("tx random", "[common][tx]")
 
 	const auto tx = TX(inputs, outputs, witnesses, heightMin, heightMax, recentBlockHeight, recentBlockHash);
 
-	REQUIRE_THAT("46819a2b140c0917513ce0fef454f9855752accbb31c16e5a3c9b8568477bb9c", Equals(tx.getId().toHex()));
-	REQUIRE(1 == tx.getInputs().size());
-	REQUIRE(1 == tx.getOutputs().size());
-	// TODO add signature
-	REQUIRE(tx.getWitnesses().empty());
+	REQUIRE_THAT("c1d8fdfd67f9382353e79d2e54df0fb1cf5ae0d46570b5dd369e772e91789466", Equals(tx.getId().toHex()));
+	REQUIRE(1u == tx.getInputs().size());
+	REQUIRE(1u == tx.getOutputs().size());
+	REQUIRE(1u == tx.getWitnesses().size());
 	REQUIRE(42u == tx.getHeightMin());
 	REQUIRE(43u == tx.getHeightMax());
 	REQUIRE(7u == tx.getRecentBlockHeight());
 	REQUIRE("5f16f4c7f149ac4f9510d9cf8cf384038ad348b3bcdc01915f95de12df9d1b02" == tx.getRecentBlockHash().toHex());
 	const auto golden = std::string(
-		"46819a2b140c0917513ce0fef454f9855752accbb31c16e5a3c9b8568477bb9c" // tx id
+		"c1d8fdfd67f9382353e79d2e54df0fb1cf5ae0d46570b5dd369e772e91789466" // tx id
 		"00000001" // input count
         "5f16f4c7f149ac4f9510d9cf8cf384038ad348b3bcdc01915f95de12df9d1b020000002a00000000" // inputs
 		"00000001" // output count
         "00000001deadbeefdeadbeefdeadbeefdeadbeefdeadbeef000000000000000000000000000000000000000000000000000000000000002a018a40bfaa73256b60764c1bf40675a99083efb075" // outputs
-		"00000000" // witnesses count
+		"00000001" // witnesses count
+        "5385685eef5fe0f2f93fa022002bb00f8b2478b9567618e47463cda72a78dcdb56a7a6bcd0bd79f709f15e0d3d53ba9909d4a3d100e25065bc0b7bd2537f39191c" // witnesses
 		"000000000000002a" // height min
 		"000000000000002b" // height max
 		"0000000000000007" // recent block height
 		"5f16f4c7f149ac4f9510d9cf8cf384038ad348b3bcdc01915f95de12df9d1b02" // recent block hash
 	);
 	REQUIRE_THAT(golden, Equals(tx.toHex()));
+}
+
+TEST_CASE("tx signed", "[common][tx]")
+{
+	// TODO implement
 }
