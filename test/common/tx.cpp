4517cbd2422450a2bbb0e902011d23395e803096
John Adler <john.adler@protonmail.ch>
2019-04-22 13:10:21 -0400

    Add zero unit test for tx.

--- test/common/tx.cpp
+++ test/common/tx.cpp
@@ -5,3 +5,23 @@
 
 using namespace ech;
 using Catch::Matchers::Equals;
+
+TEST_CASE("tx zero", "[common][tx]")
+{
+	const auto inputs = std::vector<Input>{};
+	const auto outputs = std::vector<TXO>{};
+	const auto witnesses = std::vector<crypto::Signature>{};
+	const uint64_t heightMin = 0u;
+	const uint64_t heightMax = 0u;
+	const uint64_t recentBlockHeight = 0u;
+	const auto recentBlockHash = crypto::Digest();
+
+	const auto tx = TX(inputs, outputs, witnesses, heightMin, heightMax, recentBlockHeight, recentBlockHash);
+	REQUIRE(tx.getInputs().empty());
+	REQUIRE(tx.getOutputs().empty());
+	REQUIRE(tx.getWitnesses().empty());
+	REQUIRE(0u == tx.getHeightMin());
+	REQUIRE(0u == tx.getHeightMax());
+	REQUIRE(0u == tx.getRecentBlockHeight());
+	REQUIRE("290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563" == tx.getRecentBlockHash().toHex());
+}
