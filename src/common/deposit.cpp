375583e4edaceb4a95d107bf191efed4d29bf175
John Adler <john.adler@protonmail.ch>
2019-04-22 21:33:42 -0400

    Implement deposit class.

--- src/common/deposit.cpp
+++ src/common/deposit.cpp
@@ -2,7 +2,46 @@
 
 using namespace ech;
 
+const std::vector<std::byte> Deposit::serializeData(const crypto::Address& owner, const CoinAmount amount, const Color& color, const uint64_t height, const uint64_t nonce)
+{
+	std::vector<std::byte> serial;
+
+	serial.insert(serial.end(), owner.begin(), owner.end());
+
+	const auto amountBytes = Serializable::serialize(amount);
+	serial.insert(serial.end(), amountBytes.begin(), amountBytes.end());
+
+	const auto colorSerial = color.serialize();
+	serial.insert(serial.end(), colorSerial.begin(), colorSerial.end());
+
+	const auto heightBytes = Serializable::serialize(height);
+	serial.insert(serial.end(), heightBytes.begin(), heightBytes.end());
+
+	const auto nonceBytes = Serializable::serialize(nonce);
+	serial.insert(serial.end(), nonceBytes.begin(), nonceBytes.end());
+
+	return serial;
+}
+
+Deposit::Deposit(const crypto::Address& owner, const CoinAmount amount, const Color& color, const uint64_t height, const uint64_t nonce)
+	: _source(serializeData(owner, amount, color, height, nonce))
+	, _owner(owner)
+	, _amount(amount)
+	, _color(color)
+	, _height(height)
+	, _nonce(nonce)
+{
+	// TODO double-check source
+}
+
 const std::vector<std::byte> Deposit::serialize() const
 {
-	return std::vector<std::byte>();
+	std::vector<std::byte> serial;
+
+	serial.insert(serial.end(), _source.begin(), _source.end());
+
+	const auto serialData = serializeData(_owner, _amount, _color, _height, _nonce);
+	serial.insert(serial.end(), serialData.begin(), serialData.end());
+
+	return serial;
 }
--- src/common/deposit.hpp
+++ src/common/deposit.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 // Project includes
+#include "color.hpp"
 #include "serializable.hpp"
 
 namespace ech
@@ -12,7 +13,28 @@ namespace ech
 class Deposit : public Serializable
 {
 private:
+	// Source is the receipt hash from the deposit on the parent chain, which
+	// should be identical to the hash of the serialized data here.
+	const crypto::Digest _source;
+	const crypto::Address _owner;
+	const CoinAmount _amount;
+	const Color _color;
+	const uint64_t _height;
+	// Each deposit within a block has a unique nonce, starting at 0 and incrementing
+	const uint64_t _nonce;
+
+	[[nodiscard]] static const std::vector<std::byte> serializeData(const crypto::Address& owner, const CoinAmount amount, const Color& color, const uint64_t height, const uint64_t nonce);
+
 public:
+	Deposit(const crypto::Address& owner, const CoinAmount amount, const Color& color, const uint64_t height, const uint64_t nonce);
+
+	[[nodiscard]] const auto& getSource() const { return this->_source; }
+	[[nodiscard]] const auto& getOwner() const { return this->_owner; }
+	[[nodiscard]] const auto getAmount() const { return this->_amount; }
+	[[nodiscard]] const auto isColored() const { return this->_color.isColored(); }
+	[[nodiscard]] const auto& getColor() const { return this->_color; }
+	[[nodiscard]] const auto getHeight() const { return this->_height; }
+
 	[[nodiscard]] const std::vector<std::byte> serialize() const override;
 };
 
