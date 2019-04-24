bedf9b47e28016bdf329a09d398e9845c2750dda
John Adler <john.adler@protonmail.ch>
2019-04-24 13:48:38 -0400

    Add unit tests for outpoint deserialization.

--- test/common/outpoint.cpp
+++ test/common/outpoint.cpp
@@ -19,6 +19,10 @@ TEST_CASE("outpoint testing 0", "[common][outpoint]")
 	const auto outpoint = Outpoint(TXID(digest), 0u);
 
 	REQUIRE_THAT("5f16f4c7f149ac4f9510d9cf8cf384038ad348b3bcdc01915f95de12df9d1b0200000000", Equals(outpoint.toHex()));
+
+	auto serialized = outpoint.serialize();
+	const auto deserialized = Outpoint::deserialize(serialized);
+	REQUIRE_THAT(outpoint.toHex(), Equals(deserialized.toHex()));
 }
 
 TEST_CASE("outpoint testing 42", "[common][outpoint]")
@@ -27,4 +31,15 @@ TEST_CASE("outpoint testing 42", "[common][outpoint]")
 	const auto outpoint = Outpoint(TXID(digest), 42u);
 
 	REQUIRE_THAT("5f16f4c7f149ac4f9510d9cf8cf384038ad348b3bcdc01915f95de12df9d1b020000002a", Equals(outpoint.toHex()));
+
+	auto serialized = outpoint.serialize();
+	const auto deserialized = Outpoint::deserialize(serialized);
+	REQUIRE_THAT(outpoint.toHex(), Equals(deserialized.toHex()));
+}
+
+TEST_CASE("outpoint deserialize short", "[common][outpoint]")
+{
+	auto bad1 = std::vector<std::byte>{};
+
+	REQUIRE_THROWS_AS(Outpoint::deserialize(bad1), std::runtime_error);
 }
