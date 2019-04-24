494c2b7e533bc8fd5984c0f9754eb90b22b42a9b
John Adler <john.adler@protonmail.ch>
2019-04-24 15:58:10 -0400

    Add skeleton for unit tests for state.

---
+++ test/echd/state.cpp
@@ -0,0 +1,11 @@
+#include "catch2/catch.hpp"
+
+// Project includes
+#include "echd/state.hpp"
+
+using namespace ech;
+using Catch::Matchers::Equals;
+
+TEST_CASE("state zero", "[daemon][state]")
+{
+}
