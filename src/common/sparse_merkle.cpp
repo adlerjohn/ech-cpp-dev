a58d0fd28c12c27f54c67af711984d616ef3edd8
John Adler <john.adler@protonmail.ch>
2019-05-01 13:32:17 -0400

    Add skeleton methods for SMT.

--- src/common/sparse_merkle.cpp
+++ src/common/sparse_merkle.cpp
@@ -2,7 +2,47 @@
 
 using namespace ech;
 
-const auto SparseMerkleTree::emptyNode()
+SMTNode::SMTNode(const crypto::Digest& digest)
+	: _digest(digest)
+{
+}
+
+const crypto::Digest SparseMerkleTree::defaultNodeValue()
 {
 	return crypto::Digest("0000000000000000000000000000000000000000000000000000000000000000", true);
+}
+
+SparseMerkleTree::SparseMerkleTree()
+	: _root(std::make_unique<SMTNode>(defaultNodeValue()))
+{
+}
+
+const void SparseMerkleTree::insert(const crypto::Digest& value)
+{
+	return;
+}
+
+const void SparseMerkleTree::erase(const crypto::Digest& value)
+{
+	return;
+}
+
+const auto SparseMerkleTree::getInclusionProof(const crypto::Digest& key) const
+{
+	return nullptr;
+}
+
+const auto SparseMerkleTree::getExclusionProof(const crypto::Digest& key) const
+{
+	return nullptr;
+}
+
+const bool SparseMerkleTree::checkInclusionProof(const std::vector<std::pair<bool, crypto::Digest>>& path) const
+{
+	return 0;
+}
+
+const bool SparseMerkleTree::checkExclusionProof(const std::vector<std::pair<bool, crypto::Digest>>& path) const
+{
+	return 0;
 }
--- src/common/sparse_merkle.hpp
+++ src/common/sparse_merkle.hpp
@@ -6,16 +6,47 @@
 namespace ech
 {
 
+class SMTNode
+{
+private:
+	crypto::Digest _digest;
+
+	std::unique_ptr<SMTNode> _left = nullptr;
+	std::unique_ptr<SMTNode> _right = nullptr;
+
+public:
+	explicit SMTNode(const crypto::Digest& digest);
+
+	[[nodiscard]] const auto& getDigest() const { return this->_digest; }
+};
+
 class SparseMerkleTree
 {
 private:
+	std::unique_ptr<SMTNode> _root;
+
 	/**
 	 * Optimization: hash(0, 0) = 0, so we don't need to pre-compute defaults.
-	 * @return Digest for empty node.
+	 * @return Digest for default node.
 	 */
-	[[nodiscard]] static const auto emptyNode();
+	[[nodiscard]] static const crypto::Digest defaultNodeValue();
 
 public:
+	SparseMerkleTree();
+
+	[[nodiscard]] const auto getRoot() const { return this->_root->getDigest(); }
+
+	const void insert(const crypto::Digest& key);
+
+	const void erase(const crypto::Digest& key);
+
+	[[nodiscard]] const auto getInclusionProof(const crypto::Digest& key) const;
+
+	[[nodiscard]] const auto getExclusionProof(const crypto::Digest& key) const;
+
+	[[nodiscard]] const bool checkInclusionProof(const std::vector<std::pair<bool, crypto::Digest>>& path) const;
+
+	[[nodiscard]] const bool checkExclusionProof(const std::vector<std::pair<bool, crypto::Digest>>& path) const;
 };
 
 } // namespace ech
