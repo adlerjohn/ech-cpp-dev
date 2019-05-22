ed8e34cf9f66cf8c84d4977f34c5b11c41827f77
John Adler <john.adler@protonmail.ch>
2019-05-22 18:59:52 -0400

    Implement basic methods for SMT

--- src/common/sparse_merkle.cpp
+++ src/common/sparse_merkle.cpp
@@ -2,6 +2,24 @@
 
 using namespace ech;
 
+const auto SparseMerkleTree::keyToPath(const crypto::Digest& key)
+{
+	std::vector<bool> path;
+
+	static const auto zero = std::byte(0);
+	static const auto one = std::byte(1);
+
+	path.reserve(crypto::Digest::size() * 8u);
+	for (const auto& byte : key) {
+		// 8 bits in a byte
+		for (auto i = 0u; i < 8u; i++) {
+			path.push_back((byte & one << i) == zero);
+		}
+	}
+
+	return path;
+}
+
 SMTNode::SMTNode(const crypto::Digest& digest)
 	: _digest(digest)
 {
@@ -17,32 +35,64 @@ SparseMerkleTree::SparseMerkleTree()
 {
 }
 
-const void SparseMerkleTree::insert(const crypto::Digest& value)
+const bool SparseMerkleTree::at(const crypto::Digest& key) const
+{
+	if (_root == nullptr) {
+		return false;
+	}
+
+	const auto path = keyToPath(key);
+	auto val = false;
+
+	auto node = _root.get();
+	for (const auto& isLeft : path) {
+		if (node->isLeaf()) {
+			if (node->getDigest() == key) {
+				val = true;
+			}
+			break;
+		}
+
+		if (isLeft) {
+			node = node->getLeft().get();
+		} else {
+			node = node->getRight().get();
+		}
+	}
+
+	return val;
+}
+
+const void SparseMerkleTree::insert(const crypto::Digest& key)
 {
 	return;
 }
 
-const void SparseMerkleTree::erase(const crypto::Digest& value)
+const void SparseMerkleTree::erase(const crypto::Digest& key)
 {
 	return;
 }
 
 const auto SparseMerkleTree::getInclusionProof(const crypto::Digest& key) const
 {
-	return nullptr;
+	std::vector<std::optional<crypto::Digest>> path;
+
+	return path;
 }
 
 const auto SparseMerkleTree::getExclusionProof(const crypto::Digest& key) const
 {
-	return nullptr;
+	std::vector<std::optional<crypto::Digest>> path;
+
+	return path;
 }
 
-const bool SparseMerkleTree::checkInclusionProof(const std::vector<std::pair<bool, crypto::Digest>>& path) const
+const bool SparseMerkleTree::checkInclusionProof(const std::vector<bool>& path, const std::vector<std::optional<crypto::Digest>>& hashes) const
 {
-	return 0;
+	return false;
 }
 
-const bool SparseMerkleTree::checkExclusionProof(const std::vector<std::pair<bool, crypto::Digest>>& path) const
+const bool SparseMerkleTree::checkExclusionProof(const std::vector<bool>& path, const std::vector<std::optional<crypto::Digest>>& hashes) const
 {
-	return 0;
+	return false;
 }
--- src/common/sparse_merkle.hpp
+++ src/common/sparse_merkle.hpp
@@ -11,15 +11,30 @@ class SMTNode
 private:
 	crypto::Digest _digest;
 
+	std::unique_ptr<SMTNode> _parent = nullptr;
 	std::unique_ptr<SMTNode> _left = nullptr;
 	std::unique_ptr<SMTNode> _right = nullptr;
 
 public:
 	explicit SMTNode(const crypto::Digest& digest);
 
-	[[nodiscard]] const auto& getDigest() const { return this->_digest; }
+	~SMTNode() {
+		_left.reset();
+		_right.reset();
+	}
+
+	[[nodiscard]] const auto& getDigest() const { return _digest; }
+	[[nodiscard]] const bool hasLeft() const { return _left == nullptr; }
+	[[nodiscard]] const bool hasRight() const { return _right == nullptr; }
+	[[nodiscard]] const bool isLeaf() const { return !(hasLeft() || hasRight()); }
+	[[nodiscard]] const auto& getLeft() const { return _left; }
+	[[nodiscard]] const auto& getRight() const { return _right; }
 };
 
+/**
+ * Optimized sparse Merkle tree structure. Represent subtrees with one leaf as
+ * just a single node with metadata.
+ */
 class SparseMerkleTree
 {
 private:
@@ -31,22 +46,30 @@ private:
 	 */
 	[[nodiscard]] static const crypto::Digest defaultNodeValue();
 
+	[[nodiscard]] static const auto keyToPath(const crypto::Digest& key);
+
 public:
 	SparseMerkleTree();
 
-	[[nodiscard]] const auto getRoot() const { return this->_root->getDigest(); }
+	~SparseMerkleTree() {
+		_root.reset();
+	}
+
+	[[nodiscard]] const auto getRoot() const { return _root->getDigest(); }
 
 	const void insert(const crypto::Digest& key);
 
 	const void erase(const crypto::Digest& key);
 
+	[[nodiscard]] const bool at(const crypto::Digest& key) const;
+
 	[[nodiscard]] const auto getInclusionProof(const crypto::Digest& key) const;
 
 	[[nodiscard]] const auto getExclusionProof(const crypto::Digest& key) const;
 
-	[[nodiscard]] const bool checkInclusionProof(const std::vector<std::pair<bool, crypto::Digest>>& path) const;
+	[[nodiscard]] const bool checkInclusionProof(const std::vector<bool>& path, const std::vector<std::optional<crypto::Digest>>& hashes) const;
 
-	[[nodiscard]] const bool checkExclusionProof(const std::vector<std::pair<bool, crypto::Digest>>& path) const;
+	[[nodiscard]] const bool checkExclusionProof(const std::vector<bool>& path, const std::vector<std::optional<crypto::Digest>>& hashes) const;
 };
 
 } // namespace ech
