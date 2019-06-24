dd22850c7b39f16cd8848057caec01e47849efdb
John Adler <john.adler@protonmail.ch>
2019-06-24 18:57:41 -0400

    Add unit tests for zero padding removal for eth jsonrpc client.

---
+++ test/echd/jsonrpc/eth_client.cpp
@@ -0,0 +1,42 @@
+#include "catch2/catch.hpp"
+
+// Project includes
+#include "echd/jsonrpc/eth_client.hpp"
+
+using namespace ech;
+using Catch::Matchers::Equals;
+
+TEST_CASE("eth jsonrpc client leading zeroes empty", "[daemon][jsonrpc]")
+{
+	const auto s = eth::JsonHelper::removeLeadingZeroes(std::string(""));
+
+	REQUIRE(std::string_view("0") == s);
+}
+
+TEST_CASE("eth jsonrpc client leading zeroes zero", "[daemon][jsonrpc]")
+{
+	const auto s = eth::JsonHelper::removeLeadingZeroes(std::string("0"));
+
+	REQUIRE(std::string_view("0") == s);
+}
+
+TEST_CASE("eth jsonrpc client leading zeroes padded zero", "[daemon][jsonrpc]")
+{
+	const auto s = eth::JsonHelper::removeLeadingZeroes(std::string("0000"));
+
+	REQUIRE(std::string_view("0") == s);
+}
+
+TEST_CASE("eth jsonrpc client leading zeroes non-zero", "[daemon][jsonrpc]")
+{
+	const auto s = eth::JsonHelper::removeLeadingZeroes(std::string("10"));
+
+	REQUIRE(std::string_view("10") == s);
+}
+
+TEST_CASE("eth jsonrpc client leading zeroes padded non-zero", "[daemon][jsonrpc]")
+{
+	const auto s = eth::JsonHelper::removeLeadingZeroes(std::string("0010"));
+
+	REQUIRE(std::string_view("10") == s);
+}
